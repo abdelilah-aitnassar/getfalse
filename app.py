fe8ed32264b28ed10b44231277f45f358a9aae1c
abdelilah 9090 <85766517+abdelilah-aitnassar@users.noreply.github.com>
2024-10-06 12:37:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def get_data():
         return jsonify({"error": "An unexpected error occurred."}), 500  # Internal Server Error
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(debug=True)
