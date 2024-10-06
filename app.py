32af59004a9676a6bb464d4767b390c4d720e9bc
abdelilah 9090 <85766517+abdelilah-aitnassar@users.noreply.github.com>
2024-10-06 11:32:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
 @app.route('/api/getdata', methods=['GET'])
 def get_data():
     access_token = request.args.get('accessToken')
-    
-    if not access_token:
-        return jsonify({"error": "Access token is required"}), 400
-
     url = "https://app.addtowallet.co/api/card/get?deleted=false"
+    
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
         'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
         'accessToken': access_token,
+        'Connection': 'keep-alive',
+        'Referer': 'https://app.addtowallet.co/dashboard',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'If-None-Match': 'W/cf2-TqWDa+EvD6nexyBeysl55OTEwmQ',
+        'TE': 'trailers'
     }
 
     try:
         response = requests.get(url, headers=headers)
-        # Log the response status and content
-        print(f"Response Status: {response.status_code}")
-        print(f"Response Content: {response.text}")
-        
-        response.raise_for_status()  # Raises HTTPError for bad responses
-        data = response.json()
-        return jsonify(data)
+        response.raise_for_status()  # Raises HTTPError for 4xx and 5xx responses
+        return jsonify(response.json())
     except requests.exceptions.HTTPError as err:
-        return jsonify({"error": str(err)}), response.status_code
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        app.logger.error(f"Error: {err}, Response: {response.text}")  # Log the error
+        return jsonify({"error": str(err), "response": response.text}), 403
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
