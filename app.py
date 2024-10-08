c60394dd6be98414c3f93ca3fad8f4f76aff83a2
abdelilah 9090 <85766517+abdelilah-aitnassar@users.noreply.github.com>
2024-10-08 13:06:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,25 +14,18 @@ def get_data():
     if not access_token:
         return jsonify({"error": "Access token is required"}), 400  # Bad Request if no token is provided
     
-    url = "https://app.addtowallet.co/api/card/get?deleted=false"
-    
+    url = "https://catfact.ninja/fact"
+
+    payload = {}
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
-        'accessToken': access_token,  # Include the access token in the headers
-        'Connection': 'keep-alive',
-        'Referer': 'https://app.addtowallet.co/dashboard',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'If-None-Match': 'W/cf2-TqWDa+EvD6nexyBeysl55OTEwmQ',
-        'TE': 'trailers'
+    'Cookie': '8EKCbfgSkXePBvY6IyF6KFfThGZ8snukUvFpwbW1=eyJpdiI6Ik12bTV4a1kwYTRCSnNvajI0L2VDRlE9PSIsInZhbHVlIjoib3JFTkpKRHhsU3k0ZGw5SVZCSWVldmw3Q3VSU21TUThnaWZxNW1OU3RSNFh3N3l6ZStSRXJiY2hkdnZad21xNnBYQ2tZMmowdEJRVUhVQkVxdXFyWkdjbklCbmlNQUNabGYrQVlQNnI5dmVBMkpxeERrU1lTMFdQbDUwUDRBcktoRTN6SEowcjM5UENRdnFvY1MvZXR3YUQ0ZEgwWDFCMWRlNGFHSUE2M1JQdjFVdHc0N052SFQ2ME15RW51V05FK2x4S0NHeFpRbFE1eUl3anlTa3pYQ3NmTk5TcnRhRzlXTHQxeGw1bkRaMjU1Nm5ZbmRxQ3lwRTVVUUNuY3g3TnZqZDVQWVNGZHVHZVIyK0tleml1aXpVa0VEUXBrVUxUQlhsK1lYcUJON25LNVpxcUUxTktmZSs4bDBMWVhvQVlIbVFMVms1UHNlVzV1N0FGR2prT3EvanlKd0tOSnVFZS9aQmFmRFJFakMzQ2w1TDdnTzk4VnNFMjZEbGkzWEJrIiwibWFjIjoiNWZlNTcxMGE1ZDJjYzQwM2IzNWNlZGQyMDZmZjU1YjM3NGU5N2U4N2I2NGRjNmMwYjk4NGE5M2E2YjY5OWRmYiIsInRhZyI6IiJ9; XSRF-TOKEN=eyJpdiI6IndQR09MODgrTm5xTys4REtaODg2NVE9PSIsInZhbHVlIjoia3YxMnFTM0s5TS9Bd3E2YmZmbGJ4UjlqcVUrU0VjMncyejl3YlBJK3MxeTl3WlEvNzh6dE94M3l3U2RFN3BXemJTYzBHZ08yeWNhakZPc01FYkhpb053ZHkwM2s3VzMvWkNFb1JQaTFiOXorb1NKZEd3Q2VkRzVzUVRDTGtFVzEiLCJtYWMiOiJjMjJmMjVhYjIwYWRiMTc3ODU4MjE0ODdjZmZhZGY5NDM3NDg4NTU5MTljN2U3NzYzNTMwMzVhY2E3OTcwN2ZkIiwidGFnIjoiIn0%3D; catfacts_session=eyJpdiI6IlIxZ3BEcHdWZjVNeSt6ZXRpWk9ySEE9PSIsInZhbHVlIjoiakNBVUEzaGwwa3QzUkpmay9DeCtGOWJnUHBmc0F6S0Q1bkxMWVc4UzRvWlZNcFVnN3VjMWFoMWJPenZEQ2gzRW5PYkVVeWlTNFVKTFRKZUhBcVBnYVNoc2IxNVFyYy9KTEZtU0dhdmc4eFd1cnFVWXIvK1FSWnJQRnAzMEllWG8iLCJtYWMiOiIyZGE4NmUwZTk3ZmQzMDE5MDI5MjllOGQxNjQ5ZGViMTJiMjlhNDhkMDE1ZjQ5YWE2NGY2OGZkOTQxOTc5ZjAzIiwidGFnIjoiIn0%3D'
     }
 
+    
+
+
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.request("GET", url, headers=headers, data=payload)
         response.raise_for_status()  # Raises HTTPError for 4xx and 5xx responses
         return jsonify(response.json())  # Return the JSON response from the API
     except requests.exceptions.HTTPError as err:
